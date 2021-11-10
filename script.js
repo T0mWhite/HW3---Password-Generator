ec5ce534a3ae2793d4c0b84fccbe237aa7bc934f
T0mWhite <tbw2.mail@gmail.com>
2021-11-10 17:35:42 -0600

    created arrays

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function generatePassword() {
   return password;
 }
 
+// Lowercase alphabet array
+
+let lowerAlpha = "abcdefghijklmnopqrstuvwxyz".split("");
+console.log(lowerAlpha);
+let upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+console.log(upperAlpha);
+let specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
+console.log(specialChars);
+let numbers = "1234567890".split("");
+console.log(numbers);
+
+
+
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+

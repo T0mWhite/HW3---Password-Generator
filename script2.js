d1a17534a2152b530349305c1b39c74c96e069f8
T0mWhite <tbw2.mail@gmail.com>
2021-11-11 16:47:26 -0600

    Added comments all over

--- script2.js
+++ script2.js
@@ -1,23 +1,29 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// Password generator with length and character type options
 
-// Write password to the #password input textarea element
+// Hooks variable to html button with generate id
+let generateBtn = document.querySelector("#generate");
+
+// Adds event listener to call the writePassword function on click
+generateBtn.addEventListener("click", writePassword);
+
+// Calls the writePassword function which:
+// declares password variable as the return of generatePassword function
+// declares passwordText variable as the textarea html object
+// sets the value of the passwordText object = password variable (return of generatePassword function).
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
     passwordText.value = password;
-    // concatArrays();
 }
 
 // Lowercase alphabet array
-let lowerAlpha = "abcdefghijklmnopqrstuvwxyz".split("");
+const lowerAlpha = "abcdefghijklmnopqrstuvwxyz".split("");
 // Capital alphabet array
-let upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 // Special array
-let specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
+const specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
 // Number array
-let numbers = "1234567890".split("");
+const numbers = "1234567890".split("");
 
 // Setting up variables to represent the boolean value of checkboxes
 let lowerCheck = document.getElementById("lower");
@@ -26,39 +32,35 @@ let numberCheck = document.getElementById("number");
 let specialCheck = document.getElementById("special");
 let pwLength = document.getElementById("pwLength");
 
-// Creates and empty array to concatenate the checkbox arrays into.
+//  If no boxes are checked, prompts the user to check a box and returns out of the function.If one or more boxes are checked, concatenates the checked array(s) to the selectedParameters array.
+// Then uses the .length of selectedParameters from concatArrays() to generate a random number, adds this to an index of the new variable randomArray, and repeats until the user selected character limit has been reached.
+// Function return is a string made from joining the indexes of randomArray, creating a randomly generated string of characters.
 
-// Function that concatenates an array to the selectedParameters array matching the checkbox parameters if the checkbox is selected. Returns selectedParameters after all possible concatenations are run.
 function generatePassword() {
     let selectedParameters = [];
-    randomArray = [];
-    if (lowerCheck.checked && upperCheck.checked && specialCheck.checked && numberCheck.checked === false) {
+    if (!lowerCheck.checked && !upperCheck.checked && !specialCheck.checked && !numberCheck.checked) {
+        console.log("Inside the statement");
         alert("Please choose at least 1 parameter.");
         return;    
     } if (lowerCheck.checked === true) {
-        console.log(lowerCheck.checked);
+        console.log("Lowercase checked: " + lowerCheck.checked);
         selectedParameters = selectedParameters.concat(lowerAlpha);
     } if (upperCheck.checked === true) {
-        console.log(upperCheck.checked);
+        console.log("Uppercase checked: " + upperCheck.checked);
         selectedParameters = selectedParameters.concat(upperAlpha);
     } if (specialCheck.checked === true) {
-        console.log(specialCheck.checked);
+        console.log("Special characters checked: " + specialCheck.checked);
         selectedParameters = selectedParameters.concat(specialChars);
     } if (numberCheck.checked === true) {
-        console.log(numberCheck.checked);
+        console.log("Numbers checked: " + numberCheck.checked);
         selectedParameters = selectedParameters.concat(numbers);
     }
-    
+
     for (let i = 0; i < pwLength.valueAsNumber; i++) {
         let randomNumber = Math.floor(Math.random()*selectedParameters.length);
-        randomArray = randomArray.concat(selectedParameters[randomNumber]);
+        let randomArray = randomArray.concat(selectedParameters[randomNumber]);
     }
     return randomArray.join('');
 }
-
-// Function that establishes a random array. Uses the length of the returned selectedParameters from concatArrays() to generate a random number based on the number of indexes in selectedParameters. Generates the *.length* amount of indexes with random numbers in them.
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 

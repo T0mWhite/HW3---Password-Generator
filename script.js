0cdb4e201d04d8d58e8133a1a5fc6fb3e5a4a34b
T0mWhite <tbw2.mail@gmail.com>
2021-11-10 21:09:35 -0600

    random number generator using checkbox parameters working

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
-
 // Lowercase alphabet array
-
 let lowerAlpha = "abcdefghijklmnopqrstuvwxyz".split("");
 console.log(lowerAlpha);
+// Capital alphabet array
 let upperAlpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 console.log(upperAlpha);
 let specialChars = "!#$%&'()*+,-./:;<=>?@[]^_`{|}~".split("");
@@ -25,32 +24,41 @@ let lowerCheck = document.getElementById("lower");
 let upperCheck = document.getElementById("upper");
 let numberCheck = document.getElementById("number");
 let specialCheck = document.getElementById("special");
+let pwLength = document.getElementById("pwLength");
+
+let selectedParameters = [];
+concatArrays();
 
 function concatArrays() {
     if (lowerAlpha && upperAlpha && specialChars && numbers === false) {
         alert("Please choose at least 1 parameter.");
-        return;
-    } else {
-        
+        return;    
+    } if (lowerCheck.checked === true) {
+        console.log(lowerCheck.checked);
+        selectedParameters = selectedParameters.concat(lowerAlpha);
+    } if (upperCheck.checked === true) {
+        console.log(upperCheck.checked);
+        selectedParameters = selectedParameters.concat(upperAlpha);
+    } if (specialCheck.checked === true) {
+        console.log(specialCheck.checked);
+        selectedParameters = selectedParameters.concat(specialChars);
+    } if (numberCheck.checked === true) {
+        console.log(numberCheck.checked);
+        selectedParameters = selectedParameters.concat(numbers);
     }
+    return selectedParameters;
 }
 
 
 function generatePassword() {
-  return password;
+    randomArray = [];
+    for (let i = 0; i < pwLength.valueAsNumber; i++) {
+        let randomNumber = Math.floor(Math.random()*selectedParameters.length);
+        randomArray = randomArray.concat(selectedParameters[randomNumber]);
+        console.log(i)
+    }
+    return randomArray.join('');
 }
-
-
-// if (upBox.checked === true) {
-//     upperAlpha = true;
-//     if (lwBox.checked === true)
-//     lowerAlpha = true;
-//     if (nmBox.checked === true)
-//     numBer = true;
-//     if (spBox.checked === true)
-//     SpecChar = true; 
-//   }
-
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
